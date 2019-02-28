76bf717b1ab59eb44fe5fea4ebc34b4665030bab
Mads H. Danquah <mads@danquah.dk>
2019-02-28 23:58:34 +0100

    Update the validator to support multiple schemas

--- validate.js
+++ validate.js
@@ -1,27 +1,32 @@
 const Ajv = require('ajv')
 const fs = require('fs')
 
-const schema = JSON.parse(fs.readFileSync('schema/bibob.jsonschema', 'utf8'))
-const ajv = new Ajv({'allErrors': true})
-const validate = ajv.compile(schema)
+fs.readdir('schema', function (err, items) {
+    let ajv = new Ajv({ 'allErrors': true })
+    for (var i = 0; i < items.length; i++) {
+        let schema = JSON.parse(fs.readFileSync(`schema/${items[i]}`, 'utf8'))
+        ajv = ajv.addSchema(schema)
+    }
 
-fs.readdir('examples/bibob/', function (err, items) {
-  for (var i = 0; i < items.length; i++) {
-    process.stdout.write(`Validating "${items[i]}": `);
-    let source = JSON.parse(fs.readFileSync(`examples/bibob/${items[i]}`, 'utf8'))
-    const valid = validate(source)
-    if (!valid) {
-      process.stdout.write(`${validate.errors.length} error(s) found:\n`)
-      validate.errors.forEach(function(error, i) {
-        console.log(i + 1 + ":")
-        console.log(error)
-      });
-    }
-    else {
-      process.stdout.write("Valid\n")
-    }
-    process.stdout.write("\n")
-  }
+    let validate = ajv.getSchema("http://biblstandard.dk/abm/bibob/bibob.schema.json")
+    fs.readdir('examples/bibob/', function (err, items) {
+        for (var i = 0; i < items.length; i++) {
+            process.stdout.write(`Validating "${items[i]}": `);
+            let source = JSON.parse(fs.readFileSync(`examples/bibob/${items[i]}`, 'utf8'))
+            const valid = validate(source)
+            if (!valid) {
+                process.stdout.write(`${validate.errors.length} error(s) found:\n`)
+                validate.errors.forEach(function (error, i) {
+                    console.log(i + 1 + ":")
+                    console.log(error)
+                });
+            }
+            else {
+                process.stdout.write("Valid\n")
+            }
+            process.stdout.write("\n")
+        }
+    })
+
 })
- 
 
